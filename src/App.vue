7b637cf2c80667f9f024163a100a9bd5243afc09
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2023-05-23 18:30:20 +0200

    Fixing and adding new things
    Rename files, add bootstrap icons support, add subcribe form card

--- src/App.vue
+++ src/App.vue
@@ -15,20 +15,5 @@ body {
 
 #app {
   font-family: $font-family;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
