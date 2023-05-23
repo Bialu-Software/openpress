feeb050cc864c60c0ee85c7b68c81006dba907aa
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2023-05-23 18:28:49 +0200

    Delete src/src directory

--- src/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import "bootstrap-icons/font/bootstrap-icons.css"
-
-createApp(App).use(router).mount('#app')
