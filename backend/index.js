8311f9d8185544b14133cfd7275a7ad69c1dbef8
Gumernus <gumernusbussines@gmail.com>
2023-06-10 14:24:45 +0200

    added some basic routes (non functional)

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,43 @@ const express = require('express')
 const app = express()
 const config = require("../config.json").backend
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/getPosts', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/getPost', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/addPost', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/delPost', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/editPost', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/subscriberEmailsGet', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/subscriberEmailsDel', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/subscriberEmailsAdd', (req, res) => {
+  res.send('backend test')
+})
+
+app.get('/sendEmails', (req, res) => {
   res.send('backend test')
 })
 
