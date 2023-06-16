d102a51999f8cc9de7d338392a6685b3d38466ac
Michal "Gumernus" Kopčík <70224036+gumernus@users.noreply.github.com>
2023-06-16 20:42:05 +0200

    added example post

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,18 @@ const app = express()
 const config = require("../config.json").backend
 const { filter_posts } = require("./filter.js")
 
+// this is what the posts will look like in the posts.json
+//{
+//            "id": 123,
+//            "imageUrl": "https://images.unsplash.com/photo-1682208703123-8b8abb609daa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+//            "headline": "5 Easy Ways to Boost Your Productivity Today!",
+//            "text": "In this blog post, we'll share five simple and effective ways to supercharge your productivity and get more done in less time. From organizing your workspace to setting clear goals, these tips are easy to implement and will help you make the most of your day!",
+//            "html": "<h1>Test</h1>",
+//            "author": "Michal Kopčík",
+//            "tags": ["#tutorial", "#news", "#IT"],
+//            "timestamp": 1684773447
+//}
+
 // after sucessfull login sends auth token
 app.get('/login', (req, res) => {
   res.send('backend test')
