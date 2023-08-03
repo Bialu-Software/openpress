33034007e147db484ea59d3281e49e1c67bf5cb0
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2023-08-03 18:02:07 +0200

    Design has been changed

--- src/components/basics/CardPost.vue
+++ src/components/basics/CardPost.vue
@@ -1,66 +1,78 @@
 <template>
-    <div class="card">
-        <a :href="'/post?id=' + id" class="post-link">
-            <div class="card-image">
-                <div class="image" :style="'background: url(' + imageUrl + ');' + 'background-repeat:no-repeat;background-position:center center;background-size:cover;'"></div>
-            </div>
-        </a>
-        <div class="card-content">
-            <a :href="'/post?id=' + id" class="post-link">
-                <p class="date">{{ formatTimestamp(timestamp) }}</p>
-                <h2 class="headline">{{ headline }}</h2>
-                <p class="description">{{ text }}</p>
+  <div class="card">
+    <a :href="'/post?id=' + id" class="post-link">
+      <div class="card-image">
+        <div
+          class="image"
+          :style="
+            'background: url(' +
+            imageUrl +
+            ');' +
+            'background-repeat:no-repeat;background-position:center center;background-size:cover;'
+          "
+        ></div>
+      </div>
+    </a>
+    <div class="card-content">
+      <a :href="'/post?id=' + id" class="post-link">
+        <p class="date">{{ formatTimestamp(timestamp) }}</p>
+        <h2 class="headline">{{ headline }}</h2>
+        <p class="description">{{ text }}</p>
+      </a>
 
-                <div class="tags">
-                    <p class="tag" v-for="(tag) in tags" :key="tags?.indexOf(tag)">{{ tag }}</p>
-                </div>
-            </a>
+      <div class="card-footer">
+        <p class="author">
+          <img
+            class="profile-photo"
+            src="https://media.extra.cz/static/img/2020/12/c39c180-103863-sym6-2500.jpg"
+            alt="Profile photo"
+          />
+          <span class="name">{{ author }}</span>
+        </p>
 
-            <div class="card-footer">
-                <p class="author">
-                    <img class="profile-photo" src="https://media.extra.cz/static/img/2020/12/c39c180-103863-sym6-2500.jpg" alt="Profile photo">
-                    <span class="name">{{ author }}</span>
-                </p>
-
-                <div class="action-icons">
-                    <i class="bi bi-share"></i>
-                    <i class="bi bi-bookmark bookmark" @click="setItem(id)" v-if="savedPostIndex === -1"></i>
-                    <i class="bi bi-bookmark-fill" @click="setItem(id)" v-else></i>
-                </div>
-            </div>
+        <div class="action-icons">
+          <i class="bi bi-share"></i>
+          <i
+            class="bi bi-bookmark bookmark"
+            @click="setItem(id)"
+            v-if="savedPostIndex === -1"
+          ></i>
+          <i class="bi bi-bookmark-fill" @click="setItem(id)" v-else></i>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 import { formatTimestamp } from "./functions";
 import { SavedPosts } from "./classes";
 
 export default defineComponent({
-    name: 'CardPost',
-    props: {
-        id: Number,
-        imageUrl: String,
-        headline: String,
-        text: String,
-        html: String,
-        author: String,
-        tags: Array,
-        timestamp: Number
+  name: "CardPost",
+  props: {
+    id: Number,
+    imageUrl: String,
+    headline: String,
+    text: String,
+    html: String,
+    author: String,
+    tags: Array,
+    timestamp: Number,
+  },
+  data() {
+    return {
+      formatTimestamp,
+      savedPostIndex: SavedPosts.checkPost(this.id),
+    };
+  },
+  methods: {
+    setItem(id: number | undefined) {
+      SavedPosts.setItem(id);
+      this.savedPostIndex = SavedPosts.checkPost(id);
     },
-    data() {
-        return {
-            formatTimestamp,
-            savedPostIndex: SavedPosts.checkPost(this.id)
-        }
-    },
-    methods: {
-        setItem(id: number | undefined) {
-            SavedPosts.setItem(id);
-            this.savedPostIndex = SavedPosts.checkPost(id);
-        }
-    }
+  },
 });
 </script>
 
@@ -68,138 +80,127 @@ export default defineComponent({
 @import "@/assets/styles/main";
 
 .date {
-    text-transform: uppercase;
+  text-transform: uppercase;
 }
 
 .headline {
-    color: $card-headline-color;
-    font-weight: bold;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
+  color: $card-headline-color;
+  font-weight: bold;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 }
 
 p {
-    color: $card-text-color;
-    overflow: hidden;
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    -webkit-box-orient: vertical;
+  color: $card-text-color;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 }
 
 .card-image {
-    overflow: hidden;
-    position: relative;
+  overflow: hidden;
+  position: relative;
+  height: 210px;
+  width: 100%;
+  border-radius: $card-border-radius $card-border-radius 0 0;
+
+  .image {
     height: 210px;
     width: 100%;
-    border-radius: $card-border-radius $card-border-radius 0 0;
-
-    .image {
-        height: 210px;
-        width: 100%;
-        position: relative;
-        transition: transform 0.2s ease-in-out;
-    }
+    position: relative;
+    transition: transform 0.2s ease-in-out;
+  }
 }
 
 .little {
-    max-width: 400px;
+  max-width: 400px;
 }
 
 .card {
-    background-color: $card-background-color;
-    border-radius: $card-border-radius;
+  background-color: $card-background-color;
+  border-radius: $card-border-radius;
+  display: flex;
+  flex-direction: column;
+  min-width: 100px;
+  width: auto;
+  cursor: pointer;
+
+  .card-content,
+  .card-content a {
     display: flex;
     flex-direction: column;
-    min-width: 100px;
-    width: auto;
-    cursor: pointer;
+    gap: 20px;
+    text-decoration: none;
+  }
 
+  .card-content {
+    padding: 25px;
+    height: 100%;
+    justify-content: space-between;
+  }
 
-    .card-content, .card-content a {
-        display: flex;
-        flex-direction: column;
-        gap: 20px;
-        text-decoration: none;
+  &:hover {
+    .image {
+      transform: scale(1.1);
     }
-
-    .card-content {
-        padding: 25px;
-    }
-
-    &:hover {
-        .image {
-            transform: scale(1.1);
-        }
-    }
+  }
 }
 
 .card-footer {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  cursor: default;
+
+  .author {
     display: flex;
-    justify-content: space-between;
-    gap: 20px;
-    cursor: default;
+    gap: 10px;
+    align-items: center;
 
-    .author {
-        display: flex;
-        gap: 10px;
-        align-items: center;
-
-        .name {
-            font-weight: 600;
-        }
-
-        .profile-photo {
-            max-width: 40px;
-            height: 40px;
-            border-radius: 50%;
-        }
+    .name {
+      font-weight: 600;
     }
 
-    .action-icons {
-        display: flex;
-        color: $card-icon-color;
-        gap: 10px;
-        align-items: center;
-
-        i {
-            font-size: 20px;
-            transition: 0.2s;
-            cursor: pointer;
-
-            &:hover {
-                opacity: 0.6;
-            }
-        }
+    .profile-photo {
+      max-width: 40px;
+      height: 40px;
+      border-radius: 50%;
     }
-}
+  }
 
-.tags {
+  .action-icons {
     display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 13px;
+    color: $card-icon-color;
+    gap: 10px;
+    align-items: center;
 
-    .tag {
-        padding: 5px 15px;
-        background: $card-tag-background;
-        border-radius: 4px;
+    i {
+      font-size: 20px;
+      transition: 0.2s;
+      cursor: pointer;
+
+      &:hover {
+        opacity: 0.6;
+      }
     }
+  }
 }
 
 @media (max-width: 454px) {
-    .image {
-        border-radius: 10px;
-    }
+  .image {
+    border-radius: 10px;
+  }
 
-    .card {
-        padding: 20px;
-        border-radius: 0;
+  .card {
+    padding: 20px;
+    border-radius: 0;
 
-        .card-content {
-            padding: 25px 0 0 0;
-        }
+    .card-content {
+      padding: 25px 0 0 0;
     }
+  }
 }
 </style>
