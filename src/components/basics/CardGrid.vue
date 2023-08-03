80211bc19d7a60753d7746d9d7ed673ac45e4b9b
Jan Holeček <90050233+jan-holecek@users.noreply.github.com>
2023-08-03 18:01:45 +0200

    Some small changes

--- src/components/basics/CardGrid.vue
+++ src/components/basics/CardGrid.vue
@@ -1,41 +1,46 @@
 <template>
-    <div v-if="!postsArray"></div>
-    <div class="card-container" v-else>
-        <template v-for="(post) in postsArray" :key="post.id">
-            <CardPost :id="post.id" :imageUrl="post.imageUrl" :headline="post.headline" :text="post.text" :author="post.author" :timestamp="post.timestamp" :tags="post.tags"/>
-        </template>
-    </div>
+  <div v-if="!postsArray"></div>
+  <div class="card-container" v-else>
+    <template v-for="post in postsArray" :key="post.id">
+      <CardPost
+        :id="post.id"
+        :imageUrl="post.imageUrl"
+        :headline="post.headline"
+        :text="post.text"
+        :author="post.author"
+        :timestamp="post.timestamp"
+        :tags="post.tags"
+      />
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import CardPost from '@/components/basics/CardPost.vue';
+import { defineComponent } from "vue";
+import CardPost from "@/components/basics/CardPost.vue";
 
 export default defineComponent({
-    name: 'CardGrid',
-    props: [ 
-        "postsArray"
-    ],
-    components: {
-        CardPost
-    },
+  name: "CardGrid",
+  props: ["postsArray"],
+  components: {
+    CardPost,
+  },
 });
-
 </script>
 
 <style scoped lang="scss">
 @import "@/assets/styles/main";
 
 .card-container {
-    display: grid;
-    gap: $grid-gap;
-    grid-template-columns: repeat(auto-fill, minmax(355px, 1fr));
+  display: grid;
+  gap: $grid-gap;
+  grid-template-columns: repeat(auto-fill, minmax(355px, 1fr));
 }
 
 @media (max-width: 419px) {
-    .card-container {
-        display: flex;
-        flex-direction: column;
-    } 
+  .card-container {
+    display: flex;
+    flex-direction: column;
+  }
 }
 </style>
