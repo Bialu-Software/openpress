018b45c475a95248795d1cb4c27216bce3ee78b2
Gumernus <gumernusbussines@gmail.com>
2023-10-25 22:22:53 +0200

    changed the prod port

--- backend/run_build.js
+++ backend/run_build.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8080; // As the vue dev server
+const port = 80;
 
 // Serve static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, '../dist')));
