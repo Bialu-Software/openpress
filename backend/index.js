4f8255d69462b1b173713feeb6559fecbd6b3ebc
Gumernus <gumernusbussines@gmail.com>
2023-10-25 22:18:22 +0200

    fixed .env bug

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const crypto = require('crypto');
+const dotenv = require('dotenv')
 
-// Check if the secret key is set
-if (process.env.SECRET_KEY === undefined || process.env.SECRET_KEY === '') {
+// Load environment variables from .env file
+dotenv.config();
+
+// Check if the secret key is set in the environment variables
+if (!process.env.SECRET_KEY) {
   const secret_key = crypto.randomBytes(64).toString('hex');
 
-  // Check if the .env file exists, if it does, add the key into it,
-  // else create it and add the secret key
-  if (fs.existsSync('.env')) {
-    let file = fs.readFileSync('.env', 'utf8');
-
-    // Check if the secret key is already set in the .env file, if it is, replace the line,
-    // else add it
-    if (file.includes('SECRET_KEY')) {
-      file = file.replace(/SECRET_KEY=.*/g, `SECRET_KEY=${secret_key}\n`);
-    } else {
-      file += `\nSECRET_KEY=${secret_key}\n`;
-    }
-
-    fs.writeFileSync('.env', file);
-  } else {
-    fs.writeFileSync('.env', `SECRET_KEY=${secret_key}\n`);
-  }
-
-  // Update the secret key in the environment variables
+  // Update the secret key in the environment variables and in the .env file
   process.env.SECRET_KEY = secret_key;
+  fs.writeFileSync('.env', `SECRET_KEY=${secret_key}\n`, { flag: 'a' }); // Append to .env
 
   console.warn(`\x1b[33;1mWARNING\x1b[0m: server secret key was not set, a random secret key has been generated and saved to the .env file`);
 }
