92b88293608925e611ebeb10a1f7470af442a36b
Gumernus <gumernusbussines@gmail.com>
2023-10-25 21:17:02 +0200

    changes to the github actions and ports

--- backend/run_build.js
+++ backend/run_build.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 80; // Use port 80 for HTTP
+const port = 8080; // As the vue dev server
 
 // Serve static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, '../dist')));
@@ -12,7 +12,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-// Start the Express.js server
 app.listen(port, () => {
   console.log(`Build is running on port ${port}`);
 });
