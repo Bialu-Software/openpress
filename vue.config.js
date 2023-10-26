9c3d0296ee01b4e79f04f80411b0cd741ee6a835
Gumernus <gumernusbussines@gmail.com>
2023-10-26 11:49:25 +0200

    updated dev server port

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   productionSourceMap: false,
   transpileDependencies: true,
+
+  devServer: {
+    port: process.env.DEVELOPMENT_PORT || 8080,
+  },
 });
