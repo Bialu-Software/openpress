082791168335decf33787dbc1d29b853188fc762
Michal "Gumernus" Kopčík <70224036+gumernus@users.noreply.github.com>
2023-10-26 11:30:48 +0200

    Update run_build.js

--- backend/run_build.js
+++ backend/run_build.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = process.env.PRODUCTION_PORT || 80;
 
 // Serve static files from the 'dist' folder
 app.use(express.static(path.join(__dirname, '../dist')));
